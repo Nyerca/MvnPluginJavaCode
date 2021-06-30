39cfe4ac6914c290ff1893f4dab705f80b6f591c
enrico.facciani <enrico.facciani@studio.unibo.it>
2021-06-30 14:14:33 +0200

    Add feature

---
+++ src/main/java/Feature.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Feature {
+    public static void main(String[] args) {
+        try {
+            String branch = getCurrentGitBranch();
+
+            if(branch.contains("release")) {
+                branch = "master";
+            }
+            System.out.println("Current branch Feature: " + branch);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static String getCurrentGitBranch() throws IOException, InterruptedException {
+        Process process = Runtime.getRuntime().exec("git rev-parse --abbrev-ref HEAD");
+        process.waitFor();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+
+        return reader.readLine();
+    }
+
+    public static void printResults(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
