6c556dbd530fa1d6bc59944e09bd8972685e4b90
enrico.facciani <enrico.facciani@studio.unibo.it>
2021-06-30 11:01:09 +0200

    Get branch automaticamente

--- src/main/java/GitFlow.java
+++ src/main/java/GitFlow.java
@@ -1,28 +1,49 @@
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GitFlow {
     public static void main(String[] args) {
-        System.out.println("ciao");
-
-        List<String> newLines = new ArrayList<String>();
         try {
+            String branch = getCurrentGitBranch();
+            System.out.println("Current branch: " + branch);
+            List<String> newLines = new ArrayList<String>();
+
             for (String line : Files.readAllLines(Paths.get("Jenkinsfile.txt"), StandardCharsets.UTF_8)) {
-                if (line.contains("develop")) {
-                    newLines.add(line.replace("develop", ""+System.currentTimeMillis()));
+                if (line.contains("mpl")) {
+
+                    Pattern p = Pattern.compile("mpl@\\S*'", Pattern.CASE_INSENSITIVE);
+                    Matcher m = p.matcher(line);
+                    String result = m.replaceAll("mpl@" + branch + "'");
+                    newLines.add(result);
                 } else {
                     newLines.add(line);
                 }
             }
             Files.write(Paths.get("Jenkinsfile.txt"), newLines, StandardCharsets.UTF_8);
+
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public static String getCurrentGitBranch() throws IOException, InterruptedException {
+        Process process = Runtime.getRuntime().exec("git rev-parse --abbrev-ref HEAD");
+        process.waitFor();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+
+        return reader.readLine();
     }
 }
