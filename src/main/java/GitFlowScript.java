42cb01def07b15a39995963fa846755eab99470e
enrico.facciani <enrico.facciani@studio.unibo.it>
2021-06-30 17:13:34 +0200

    Aggiunto script

---
+++ src/main/java/GitFlowScript.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GitFlowScript {
+    public static void main(String[] args) {
+        try {
+            openFeature("test");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getCurrentGitBranch() throws IOException, InterruptedException {
+        Process process = Runtime.getRuntime().exec("git rev-parse --abbrev-ref HEAD");
+        process.waitFor();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+
+        return reader.readLine();
+    }
+
+    public static void printResults(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+
+    public static void openFeature(String name) throws IOException, InterruptedException {
+        executeCommand("git checkout -b feature/" + name);
+        modifyJenkinsfile("feature/" + name);
+    }
+
+    public static void closeFeature() throws IOException, InterruptedException {
+        String name = getCurrentGitBranch();
+        executeCommand("git checkout develop && git merge feature/" + name);
+        modifyJenkinsfile("develop");
+
+    }
+
+    public static void executeCommand(String command) throws IOException, InterruptedException {
+        boolean isWindows = System.getProperty("os.name")
+                .toLowerCase().startsWith("windows");
+        ProcessBuilder builder = new ProcessBuilder();
+        if (isWindows) {
+            builder.command("cmd.exe", "/c", command);
+        } else {
+            builder.command("sh", "-c", command);
+        }
+        Process process = builder.start();
+        int exitCode = process.waitFor();
+        printResults(process);
+    }
+
+
+    public static void modifyJenkinsfile(String branch) throws IOException {
+        List<String> newLines = new ArrayList<String>();
+
+        for (String line : Files.readAllLines(Paths.get("Jenkinsfile.txt"), StandardCharsets.UTF_8)) {
+            if (line.contains("mpl")) {
+
+                Pattern p = Pattern.compile("mpl@\\S*'", Pattern.CASE_INSENSITIVE);
+                Matcher m = p.matcher(line);
+                String result = m.replaceAll("mpl@" + branch + "'");
+                newLines.add(result);
+            } else {
+                newLines.add(line);
+            }
+        }
+        Files.write(Paths.get("Jenkinsfile.txt"), newLines, StandardCharsets.UTF_8);
+    }
+}
